3a86028448994d897540b3d9dda49a7fcdf83f5d
Iamneo <techadmin@iamneo.ai>
2023-08-01 07:51:15 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -43,7 +43,6 @@ function App() {
             <h1 className="text-3xl">Quizz App</h1>
             <i className="">learn react</i>
           </div>
-              {/* {start && <Button onClick={ handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>   } */}
               {banner && <Banner score={attempt}  />}
             {card && <div> {questions.map((q, index) => (
                 <Card key={index} question={q.question}  correctAnswerMarkUpdate={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer} setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsnAttempt} />            
